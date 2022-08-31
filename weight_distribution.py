84d41b16394c1a3fcf09f7e6fd339cd865da8b17
vinay2991997 <vinay2991997@gmail.com>
2022-08-31 22:58:34 +0530

    remove unwanted lines

--- weight_distribution.py
+++ weight_distribution.py
@@ -1,7 +1,7 @@
 
 from math import ceil, floor
 import random
-import item_field
+
 
 def weight_distribution_for_one(item_list, total_sum_needed, data, variance=2):
 
@@ -38,8 +38,8 @@ def weight_distribution_helper(item_list, result, total_sum_needed=[], variance=
             local_weight_list.remove(i)
 
     if len(local_weight_list) > 0:
-        print('some items are remaining : ')
-        print(local_weight_list)
+        result.clear()
+        return False
     return True
 
 
@@ -47,7 +47,6 @@ def weight_distribution(item_list, result, total_sum_needed_list=[]):
 
     max_weight_of_single_bale = 83
 
-    # total_sum_of_weight = sum(item_list)
     total_sum_of_weight = sum([item.total_weight for item in item_list])
     no_of_bales = ceil(total_sum_of_weight/max_weight_of_single_bale)
     total_sum_needed = ceil(total_sum_of_weight/no_of_bales)
@@ -66,28 +65,4 @@ def weight_distribution(item_list, result, total_sum_needed_list=[]):
     while not (weight_distribution_helper(item_list, result, total_sum_needed_list, variance)):
         fail_count += 1
         variance = floor(fail_count/fail_count_limit)
-        # if fail_count >= fail_count_limit:
-        #     variance += 1
-        #     print('changing variance to ' + str(variance))
-        #     print('fail count : ' + str(fail_count))
-        #     fail_count = 0
-    
-    # print('fail count : ' + str(fail_count))
-    # print('changing variance to ' + str(variance))
-
-    pass
-    
-
-
-# # list_of_weights = [5, 10, 25, 15, 42, 27, 23]
-# # list_of_weights = [8,16,20,24,20,12,6,16,12,13,25,12,15,14,6,2]
-# list_of_weights = [11, 13, 27, 15, 20, 18, 18, 16, 25, 12, 8, 10, 8]
-# # list_of_weights = [48,48,17,14,33,25,10,7,4,5,8,6]
-
-# result = []
-# weight_distribution(list_of_weights, result)
-
-# for data in result:
-#     print(data)
-#     print(sum(data))
-#     print()
+         
